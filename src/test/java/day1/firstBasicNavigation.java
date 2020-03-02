017083dde4a24536702f162614d0d4ff21d8ee5e
ZeynepOz <z.ozdecan@hotmail.com>
2020-03-01 23:21:45 -0500

    firstSelenium

---
+++ src/test/java/day1/firstBasicNavigation.java
@@ -0,0 +1,31 @@
+package day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstBasicNavigation {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");
+
+        Thread.sleep(2000);
+
+        driver.close();
+
+       // driver.quit();
+
+
+
+
+    }
+
+
+
+}
